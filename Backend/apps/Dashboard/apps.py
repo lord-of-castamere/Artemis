a88aa50c80e964eefd59b31f314342be0070bc85
Ed Rubio <ed-rubio@outlook.com>
2025-09-07 00:52:20 -0700

    INIT: Estructura inicial
    Organización principal de carpetas del proyecto, añadiendo la base del Backend

---
+++ Backend/apps/Dashboard/apps.py
@@ -0,0 +1,12 @@
+
+# -- APPS -------------------------------------------------------------------- #
+
+from django.apps import AppConfig
+
+# ---------------------------------------------------------------------------- #
+
+class DashboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.Dashboard'
+
+# ---------------------------------------------------------------------------- #
